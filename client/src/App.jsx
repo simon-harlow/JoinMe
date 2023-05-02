d184778b693412a24503dd0e4fd50f465e79bcc1
Simon Harlow <simonharlow11@gmail.com>
2023-05-01 21:06:03 -0700

    changed design to have menu/nav fixed on lefthand side as this will be desktop app only, styled menu and added links to other pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import Header from './components/Header/Header'
-// import Login from "./pages/Login/Login"
+import './App.scss'
+
+import Menu from './components/Menu/Menu'
+import Welcome from './components/Welcome/Welcome'
+import AllEvents from './components/AllEvents/AllEvents'
+import Event from './components/Event/Event'
+import Profile from './components/Profile/Profile'
 import NotFound from './pages/NotFound/NotFound';
-import Footer from './components/Footer/Footer';
-import Main from './pages/Main/Main';
 
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+      <div className="app-container">
+        <Menu />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/events" element={<AllEvents />} />
+            <Route path="/events/:eventId" element={<Event />} />
+            <Route path="/user/:userId" element={<Profile />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
 
 export default App;
-
---
+++ client/src/components/Menu/Menu.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import './Menu.scss'
+import JoinMeWhite from '../../assets/icons/web/joinme_logo_white.svg'
+import Run from '../../assets/icons/activity/run.svg'
+import Calendar from '../../assets/icons/web/calendar.svg'
+import Profile from '../../assets/icons/web/profile.svg'
+import Logout from '../../assets/icons/web/logout.svg'
+
+const Menu = () => {
+	return (
+		<nav className="menu">
+			<header className="menu__header">
+				<NavLink to="/" className="menu__link" >
+				<img className="menu__header-run" src={Run} alt="brainflix logo"/>
+				<img className="menu__header-logo" src={JoinMeWhite} alt="brainflix logo"/>
+				</NavLink>
+			</header>
+			<div className="menu__links">
+				<NavLink to="events" className="menu__link" >
+					<img className="menu__icon" src={Calendar} to your icon alt="icon" />
+					<span className="menu__text">Events</span>
+				</NavLink>
+				<NavLink to="/users/:userId" className="menu__link" >
+					<img className="menu__icon" src={Profile} to your icon alt="icon" />
+					<span className="menu__text">My Profile</span>
+				</NavLink>
+				<NavLink to="/login" className="menu__link" >
+					<img className="menu__icon" src={Logout} to your icon alt="icon" />
+					<span className="menu__text">Logout</span>
+				</NavLink>
+			</div>
+		</nav>
+	);
+};
+
+export default Menu;
